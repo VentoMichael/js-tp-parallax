a6ada23dca1f09fe6d8353b49ffb1e34b2697bdf
Vento <Michael.vento@student.hepl.be>
2019-12-04 14:12:55 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+(function parallax(){
+  const Parallax = {
+      init() {
+          document.documentElement.classList.add('js-enabled');
+          var parallax = document.querySelector(".parallax");
+          this.addEventListeners;
+      },
+      addEventListeners(){
+          
+      }
+  };
+  Parallax.init();
+}());
