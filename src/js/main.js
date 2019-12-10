a2e41fec16d87b814a3632aa8602cf1b0b25288e
Vento <Michael.vento@student.hepl.be>
2019-12-10 11:50:53 +0100

    Final  commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-(function parallax(){
-  const Parallax = {
-      init() {
-          document.documentElement.classList.add('js-enabled');
-          var parallax = document.querySelector(".parallax");
-          this.addEventListeners;
-      },
-      addEventListeners(){
-          
-      }
-  };
-  Parallax.init();
+(function parallax() {
+    const Parallax = {
+        init() {
+            document.documentElement.classList.add('js-enabled');
+            this.parallax = document.querySelector(".parallax");
+            Parallax.addEventListeners();
+        },
+        addEventListeners() {
+            window.addEventListener("scroll", () => {
+
+                this.scrollHeight = window.pageYOffset;
+                this.limit = this.parallax.offsetTop + this.parallax.offsetHeight;
+
+                if (this.scrollHeight <= this.limit) {
+                    this.parallax.style.backgroundPositionY = (this.scrollHeight - this.parallax.offsetTop) + "px";
+                    console.log(this.parallax.style.backgroundPositionY);
+                }
+            });
+        }
+    };
+    Parallax.init();
 }());
